d574a87232bbd9d9104e96cad99be2fb6e8f656b
Charles-patrick <ebukacharles006@gmail.com>
2025-07-17 13:24:21 +0100

    nav changes

--- components/ui/theme-button.js
+++ components/ui/theme-button.js
@@ -1,9 +1,7 @@
 "use client";
 import { Moon, Sun } from "lucide-react";
 
-export default function ThemeButton({setShowDropdown}) {
-
-
+export function ThemeButton({ setShowDropdown }) {
   const showDropdown = () => {
     setShowDropdown((prev) => !prev);
   };
@@ -12,6 +10,22 @@ export default function ThemeButton({setShowDropdown}) {
     <>
       <div>
         <button onClick={showDropdown} className="">
+          <Sun className="h-[1.2rem] w-[1.2rem] block dark:hidden -mb-1" />
+          <Moon className="h-[1.2rem] w-[1.2rem] hidden dark:block -mb-1" />
+        </button>
+      </div>
+    </>
+  );
+}
+export function ThemeButtonMobile({ setShowHamburgerDropdown }) {
+  const showHamburgerDropdown = () => {
+    setShowHamburgerDropdown((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={showHamburgerDropdown} className="">
           <Sun className="h-[1.2rem] w-[1.2rem] block dark:hidden -mb-1" />
           <Moon className="h-[1.2rem] w-[1.2rem] hidden dark:block -mb-1" />
         </button>
--- layout/Navbar.js
+++ layout/Navbar.js
@@ -1,14 +1,18 @@
 "use client";
 import { useEffect, useState } from "react";
 import { NavData } from "@/data";
-import { Home, Briefcase, Code, Send } from "lucide-react";
-import ThemeButton from "@/components/ui/theme-button";
+import { ThemeButton , ThemeButtonMobile } from "@/components/ui/theme-button";
 import { useTheme } from "next-themes";
+import Hamburger from "hamburger-react";
+
 
 export default function Navbar() {
   const [showNavbar, setShowNavbar] = useState(true);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showHamburgerDropdown, setShowHamburgerDropdown] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
   const { setTheme } = useTheme();
 
   // Detect scroll direction
@@ -25,133 +29,151 @@ export default function Navbar() {
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      setShowDropdown(false);
+      setShowHamburgerDropdown(false);
+    };
   }, [lastScrollY]);
 
   return (
-    <div
-      className={`
+    <>
+      {/*DESKTOP NAVBAR*/}
+      <div
+        className={`
         fixed top-4 left-1/2 -translate-x-1/2 z-50 transition-transform duration-300
         ${
           showNavbar ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0"
         }
       `}>
-      {/*DESKTOP NAVBAR*/}
-      <div
-        className="
+        <div
+          className="
         hidden md:flex justify-between items-center gap-4 
         shadow-md border border-gray-200 dark:border-gray-800 
         backdrop-blur-lg bg-white dark:bg-black 
         px-6 py-2 rounded-full transition-colors 
       ">
-        {NavData.map((item, idx) => (
-          <a
-            key={idx}
-            href={item.href}
-            className="
+          {NavData.map((item, idx) => (
+            <a
+              key={idx}
+              href={item.href}
+              className="
               flex items-center gap-2 text-sm font-medium
               text-black dark:text-white 
               hover:text-gray-700 dark:hover:text-gray-300 
               transition-colors
             ">
-            <span className="capitalize whitespace-nowrap">{item.label}</span>
-          </a>
-        ))}
-        <div className="relative">
-          <ThemeButton setShowDropdown={setShowDropdown} />
-          {showDropdown && (
-            <div
-              className="
+              <span className="capitalize whitespace-nowrap">{item.label}</span>
+            </a>
+          ))}
+          <div className="relative">
+            <ThemeButton setShowDropdown={setShowDropdown} />
+            {showDropdown && (
+              <div
+                className="
         absolute right-0 mt-2 w-32
         bg-white dark:bg-black border border-gray-200 dark:border-gray-700
         rounded-md shadow-md z-50 text-sm">
-              <button
-                onClick={() => {
-                  setTheme("dark");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                Dark
-              </button>
-              <button
-                onClick={() => {
-                  setTheme("light");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                Light
-              </button>
-              <button
-                onClick={() => {
-                  setTheme("system");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                System
-              </button>
+                <button
+                  onClick={() => {
+                    setTheme("dark");
+                    setShowDropdown(false);
+                  }}
+                  className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                  Dark
+                </button>
+                <button
+                  onClick={() => {
+                    setTheme("light");
+                    setShowDropdown(false);
+                  }}
+                  className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                  Light
+                </button>
+                <button
+                  onClick={() => {
+                    setTheme("system");
+                    setShowDropdown(false);
+                  }}
+                  className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                  System
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/*MOBILE NAVBAR*/}
+      <div
+        className={`
+        fixed top-4 right-[20px] z-50 transition-transform duration-300
+        ${
+          showNavbar ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0"
+        }
+      `}>
+        <div
+          className="
+        flex md:hidden justify-between items-center 
+        shadow-md border border-gray-200 dark:border-gray-800 
+        backdrop-blur-lg bg-white dark:bg-black 
+        px-3 py-0 rounded-full transition-colors 
+      ">
+          <Hamburger
+            toggled={hamburgerOpen}
+            toggle={setHamburgerOpen}
+            size={20}
+          />
+          {hamburgerOpen && (
+            <div className="absolute top-12 right-0 w-40 text-center bg-white dark:bg-black border border-gray-200 dark:border-gray-700 rounded-md shadow-md z-50">
+              {NavData.map((item, idx) => (
+                <a
+                  key={idx}
+                  href={item.href}
+                  onClick={() => setHamburgerOpen(false)}
+                  className="block px-4 py-2 text-sm text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                  {item.label}
+                </a>
+              ))}
+              <div className="border-t border-gray-200 dark:border-gray-700">
+                <ThemeButtonMobile
+                  setShowHamburgerDropdown={setShowHamburgerDropdown}
+                />
+                {showHamburgerDropdown && (
+                  <div
+                    className="mt-2 bg-white dark:bg-black rounded-b-md z-50 text-sm">
+                    <button
+                      onClick={() => {
+                        setTheme("dark");
+                        setShowHamburgerDropdown(false);
+                      }}
+                      className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                      Dark
+                    </button>
+                    <button
+                      onClick={() => {
+                        setTheme("light");
+                        setShowHamburgerDropdown(false);
+                      }}
+                      className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                      Light
+                    </button>
+                    <button
+                      onClick={() => {
+                        setTheme("system");
+                        setShowHamburgerDropdown(false);
+                      }}
+                      className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+                      System
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           )}
         </div>
       </div>
-      {/*MOBILE NAVBAR*/}
-      <div
-        className="
-        flex md:hidden justify-between items-center gap-4 
-        shadow-md border border-gray-200 dark:border-gray-800 
-        backdrop-blur-lg bg-white dark:bg-black 
-        px-6 py-2 rounded-full transition-colors 
-      ">
-        Mobile Navbar
-        {/* {NavData.map((item, idx) => (
-          <a
-            key={idx}
-            href={item.href}
-            className="
-              flex items-center gap-2 text-sm font-medium
-              text-black dark:text-white 
-              hover:text-gray-700 dark:hover:text-gray-300 
-              transition-colors
-            ">
-            <span className="capitalize whitespace-nowrap">{item.label}</span>
-          </a>
-        ))}
-        <div className="relative">
-          <ThemeButton setShowDropdown={setShowDropdown} />
-          {showDropdown && (
-            <div
-              className="
-        absolute right-0 mt-2 w-32
-        bg-white dark:bg-black border border-gray-200 dark:border-gray-700
-        rounded-md shadow-md z-50 text-sm">
-              <button
-                onClick={() => {
-                  setTheme("dark");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                Dark
-              </button>
-              <button
-                onClick={() => {
-                  setTheme("light");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                Light
-              </button>
-              <button
-                onClick={() => {
-                  setTheme("system");
-                  setShowDropdown(false);
-                }}
-                className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                System
-              </button>
-            </div>
-          )}
-        </div> */}
-      </div>
-    </div>
+    </>
   );
 }
  
